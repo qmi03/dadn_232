e81dbb34c05f714b2ca7f4bb4c3a6dd951f25082
qmi <minh.pham1126@gmail.com>
2024-04-04 02:06:15 +0700

    feat: now able to connected to adafruit server

---
+++ Backend/src/mqttserver/index.ts
@@ -0,0 +1,44 @@
+import mqtt from 'mqtt';
+import dotenv from 'dotenv';
+dotenv.config();
+// config nodemon: "start:mqtt": "nodemon -config nodemon-mqtt.json",
+
+const host = process.env.MQTT_HOST
+const port = process.env.MQTT_PORT
+const username = process.env.ADAFRUIT_USER
+const key = process.env.ADAFRUIT_KEY
+
+const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
+
+const connectUrl = `mqtt://${host}:${port}`
+
+const options = {
+  clientId: clientId,
+  clean: true,
+  connectTimeout: 4000,
+  username: username,
+  password: key,
+  reconnectPeriod: 1000,
+}
+const client = mqtt.connect(connectUrl, options)
+
+const topic2 = `${username}/feeds/bbc-temp`
+
+client.on('connect', () => {
+  console.log('Connected')
+})
+
+// client.publish(topic, 'abcxyz')
+client.subscribe(topic2, (error) => {
+  if (error) {
+    console.log(error)
+  }
+  else {
+    console.log(`Subscribed to ${topic2}`)
+  }
+})
+
+// client.publish(topic2,'testing topic test-mqtt')
+client.on('message', (topic, payload) => {
+  console.log('Received Message:', topic, payload.toString())
+})
